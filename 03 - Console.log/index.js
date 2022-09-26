e1a98b6c09a5e1718ccae691754bdcc8c8b29db8
Claudinei Gomes <60714768+ClaudHelloworld@users.noreply.github.com>
2022-09-26 10:52:03 -0300

    aula 03 - basic console log

---
+++ 03 - Console.log/index.js
@@ -0,0 +1,18 @@
+// faz aparecer uma mensagem na tela, uasr ponto e virgula é opcional.
+// é preciso envolver o texto em aspas dentro dos parentes para um valor literal.
+console.log('Hello World');
+// pode ser com aspas duplas tbm, e sempre que utilziar ela vai quebrar uma linha.
+console.log("Hello World");
+//outro meio é com crases
+console.log(`Hello World`);
+//Qual a diferença entre elas?
+//Você pode utilizar aspas simples dentro de aspas duplas ou vice versa.
+console.log("'Hello World'");
+console.log('"Hello World"');
+//já com crases você pode utilizar tanto aspas duplas quanto simples dentro porq é não está infligindo
+//a regra
+console.log(`'Hello' "World"`);
+
+//Exibir um numero voce concatena valores com uma virgula ", "
+console.log(123123, 234.333, "Luiz");
+
