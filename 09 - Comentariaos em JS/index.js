bdc2a6555078ca759ef0152a120e9a617fb3f425
Claudinei Gomes <60714768+ClaudHelloworld@users.noreply.github.com>
2022-09-26 11:08:38 -0300

    aula 09

---
+++ 09 - Comentariaos em JS/index.js
@@ -0,0 +1,5 @@
+//Exercicio exiba a seguinte frase - Meu nome é "NOME". Estou aprendendo Javascript ás 10 da manhã.
+
+//Minha solucao
+console.log ('meu nome é "Claudinei". Estou aprendendo JavaScript ás', 10,':',59, 'da manhã.');
+
